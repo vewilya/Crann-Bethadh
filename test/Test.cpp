7114ac9e4b51f593444d34a6b19255089eb446ce
Jan Wilczek <janekw@vp.pl>
2023-11-17 09:30:17 +0000

    CMake setup with GoogleTest.

---
+++ test/Test.cpp
@@ -0,0 +1,8 @@
+#include <PluginProcessor.h>
+#include <gtest/gtest.h>
+
+namespace test {
+TEST(CrannBethadhAudioProcessor, ProcessorCanBeInstantiated) {
+    CrannBethadhAudioProcessor processor{};
+}
+}
